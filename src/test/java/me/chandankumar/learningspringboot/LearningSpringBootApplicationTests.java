350354059092235901f905725fc7355584d8addc
ChandanKr16 <chandan.kumar2021@vitstudent.ac.in>
2023-02-03 17:59:59 +0530

    Created empty project

---
+++ src/test/java/me/chandankumar/learningspringboot/LearningSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package me.chandankumar.learningspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

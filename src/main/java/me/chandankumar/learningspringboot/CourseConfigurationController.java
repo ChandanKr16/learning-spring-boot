daa556c451d7d654d87dac594b3afeace477f592
ChandanKr16 <chandan.kumar2021@vitstudent.ac.in>
2023-02-03 19:32:29 +0530

    Added configuration using class and controller

--- src/main/java/me/chandankumar/learningspringboot/CourseConfigurationController.java
+++ src/main/java/me/chandankumar/learningspringboot/CourseConfigurationController.java
@@ -1,2 +1,18 @@
-package me.chandankumar.learningspringboot;public class CourseConfigurationController {
+package me.chandankumar.learningspringboot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CourseConfigurationController {
+
+    @Autowired
+    private CourseServiceConfiguration courseServiceConfiguration;
+
+    @GetMapping("/config")
+    public CourseServiceConfiguration getCourseServiceConfiguration(){
+        return courseServiceConfiguration;
+    }
+
 }
